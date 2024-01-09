6376bce5712854164f58725c5ffdecaeaf8a769f
Gerardo Lozano <Gerardolozano51@icloud.com>
2024-01-08 18:17:45 -0600

    Ajuste Redes sociales

--- src/navigation.js
+++ src/navigation.js
@@ -19,13 +19,32 @@ export const headerData = {
       href: '/contacto',
     },
   ],
+  socialLinks: [
+    {
+      ariaLabel: 'Instagram',
+      icon: 'tabler:brand-instagram',
+      href: 'https://www.instagram.com/maicronet?igsh=N3hla3k5c2QzZHNu',
+    },
+    {
+      ariaLabel: 'Facebook',
+      icon: 'tabler:brand-facebook',
+      href: 'https://www.facebook.com/maicronetmty?mibextid=ZbWKwL',
+    },
+  ],
 };
 
 export const footerData = {
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    {
+      ariaLabel: 'Instagram',
+      icon: 'tabler:brand-instagram',
+      href: 'https://www.instagram.com/maicronet?igsh=N3hla3k5c2QzZHNu',
+    },
+    {
+      ariaLabel: 'Facebook',
+      icon: 'tabler:brand-facebook',
+      href: 'https://www.facebook.com/maicronetmty?mibextid=ZbWKwL',
+    },
   ],
   footNote: `
     <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
